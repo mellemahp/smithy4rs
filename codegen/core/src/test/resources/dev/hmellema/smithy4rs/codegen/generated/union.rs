864d6543effc34c64a38de198b323c59b58701fe
Hunter Mellema <mellemahp@gmail.com>
2026-01-09 10:14:05 -0700

    Add union codgen

---
+++ codegen/core/src/test/resources/dev/hmellema/smithy4rs/codegen/generated/union.rs
@@ -0,0 +1,19 @@
+smithy!("com.test#MyUnion": {
+    union MY_UNION_SCHEMA {
+        STRING_VARIANT: STRING = "string_variant"
+        INTEGER_VARIANT: INTEGER = "integer_variant"
+        UNIT_VARIANT: UNIT_SCHEMA = "unit_variant"
+    }
+});
+
+#[smithy_union]
+#[derive(SmithyShape)]
+#[smithy_schema(MY_UNION_SCHEMA)]
+pub enum MyUnion {
+    #[smithy_schema(STRING_VARIANT)]
+    StringVariant(String),
+    #[smithy_schema(INTEGER_VARIANT)]
+    IntegerVariant(i32),
+    #[smithy_schema(UNIT_VARIANT)]
+    UnitVariant(Unit),
+}
